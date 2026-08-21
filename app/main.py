3eebc9bd1e6f1e9f5b3baab2168df5e88a6e88fe
maul <maul2821@gmail.com>
2026-08-21 22:38:24 +0700

    include card

--- app/main.py
+++ app/main.py
@@ -3,10 +3,12 @@ from fastapi import FastAPI
 from app.db.database import Base, engine
 from app.routers.auth import router as auth_router
 from app.routers.decks import router as deck_router
+from app.routers.cards import router as card_router
 
 # Import models so SQLAlchemy knows about them
 from app.models.user import User
 from app.models.deck import Deck
+
 
 
 app = FastAPI(
@@ -14,13 +16,9 @@ app = FastAPI(
     version="0.1.0",
 )
 
-
-Base.metadata.create_all(bind=engine)
-
-
 app.include_router(auth_router)
 app.include_router(deck_router)
-
+app.include_router(card_router)
 
 @app.get("/health")
 def health_check():
